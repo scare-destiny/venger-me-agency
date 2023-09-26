06082fea9bc66d4a5ffd2f88f80c2506038db879
scare-destiny <zhenya.venger@gmail.com>
2023-09-26 09:02:34 +0300

    updated made by me link

--- src/navigation.js
+++ src/navigation.js
@@ -190,7 +190,7 @@ export const footerData = {
     { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: 'https://t.me/scare_destiny' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://venger.me/" target="_blank"> Eugene Venger</a> · All rights reserved.
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://personal.venger.me/" target="_blank"> Eugene Venger</a> · All rights reserved.
     <p class="pt-2">
     <a href="mailto:eugene@venger.me" class="text-current no-underline hover:underline">
       eugene@venger.me
