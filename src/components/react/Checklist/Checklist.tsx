4de90a744b83518de6d06f63f61afb33e53cc98f
scare-destiny <zhenya.venger@gmail.com>
2023-10-09 19:03:05 +0300

    useref for counter

--- src/components/react/Checklist/Checklist.tsx
+++ src/components/react/Checklist/Checklist.tsx
@@ -1,18 +1,43 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import CheckListItem from './ChecklistItem';
 import CheckedCount from '../UI/CheckedCount';
 import { checklistItems } from 'data/checkListItems';
 
 const Checklist: React.FC = () => {
   const [checkedCount, setCheckedCount] = useState(0);
+  const [isChecklistVisible, setIsCheckListVisible] = useState(false);
+
+  const checklistRef = useRef(null);
 
   const handleCheckboxToggle = useCallback((isChecked) => {
     setCheckedCount((prevCount) => prevCount + (isChecked ? 1 : -1));
   }, []);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsCheckListVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1, // Adjust the threshold to control when the callback fires
+      }
+    );
+
+    if (checklistRef.current) {
+      observer.observe(checklistRef.current);
+    }
+
+    // Cleanup observer on component unmount
+    return () => {
+      if (checklistRef.current) {
+        observer.unobserve(checklistRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100 p-4 md:p-8">
-      <div className=" mx-auto bg-white rounded-xl shadow-md max-w-5xl ">
+      <div ref={checklistRef} className=" mx-auto bg-white rounded-xl shadow-md max-w-5xl ">
         <div className="md:flex">
           <div className="p-4">
             {checklistItems.map((item) => (
@@ -21,11 +46,13 @@ const Checklist: React.FC = () => {
           </div>
         </div>
       </div>
-      <CheckedCount
-        count={checkedCount}
-        totalCount={checklistItems.reduce((total, item) => total + item.tasks.length, 0)}
-        className="fixed bottom-4 right-4"
-      />
+      {isChecklistVisible && (
+        <CheckedCount
+          count={checkedCount}
+          totalCount={checklistItems.reduce((total, item) => total + item.tasks.length, 0)}
+          className="fixed bottom-4 right-4"
+        />
+      )}
     </div>
   );
 };
