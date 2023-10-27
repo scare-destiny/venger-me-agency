cd38e42a3fe07a8e0d189367697777fb4bbc015d
scare-destiny <zhenya.venger@gmail.com>
2023-10-27 22:20:30 +0300

    remove dark mode

--- src/components/react/Tailwind/Hero.tsx
+++ src/components/react/Tailwind/Hero.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
   { name: 'Product', href: '#' },
