c422980bcc33b07c443855b2b08f0ba50bf60635
scare-destiny <zhenya.venger@gmail.com>
2023-10-28 14:37:54 +0300

    add hedaer to marketing checklist

--- src/components/react/Tailwind/Hero.tsx
+++ src/components/react/Tailwind/Hero.tsx
@@ -5,7 +5,7 @@ export default function EmailHero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
