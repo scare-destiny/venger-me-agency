cf4428432fd0c9f6ebaa3b6744c801fb53bd3d7c
scare-destiny <zhenya.venger@gmail.com>
2023-11-20 20:48:47 +0200

    deploy ai

--- src/components/react/ICP/Form.tsx
+++ src/components/react/ICP/Form.tsx
@@ -1,66 +1,42 @@
 import React from 'react';
+import { useChat } from 'ai/react';
 
 export default function Form() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log('test');
+  const { messages, input, handleInputChange, handleSubmit } = useChat({ api: '/api/icp' });
 
-    const apiUrl = import.meta.env.PUBLIC_API_BASE_URL || 'http://localhost:4321'; // Default to localhost if the env var is not set
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   console.log('test');
 
-    const response = await fetch(`${apiUrl}/api/icp`, {
-      method: 'POST',
-      body: JSON.stringify({}),
-    }).then((result) => result.json());
+  //   const apiUrl = import.meta.env.PUBLIC_API_BASE_URL || 'http://localhost:4321'; // Default to localhost if the env var is not set
 
-    console.log(response);
-  };
+  //   const response = await fetch(`${apiUrl}/api/icp`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({}),
+  //   }).then((result) => result.json());
+
+  //   console.log(response);
+  // };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="space-y-12">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-2">
-          <div className="grid min-w-[50%] grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2 lg:mx-auto">
-            <div className="col-span-full">
-              <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
-                Website
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">https://</span>
-                  <input
-                    type="text"
-                    name="website"
-                    id="website"
-                    className="block flex-1   border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="www.example.com"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="col-span-full">
-              <label htmlFor="ICP" className="block text-sm font-medium leading-6 text-gray-900">
-                Your Ideal Customer Profile
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="ICP"
-                  name="ICP"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-            </div>
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      <form onSubmit={handleSubmit}>
+        <input
+          className=" w-full max-w-md  border border-gray-300 rounded my-12 shadow-xl p-2 dark:text-black"
+          value={input}
+          placeholder="Describe your business..."
+          onChange={handleInputChange}
+        />
+      </form>
+      {messages.length > 0 ? (
+        messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
           </div>
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary mx-auto min-w-[20%] hover:text-link dark:hover:text-white px-4 py-3 flex items-center"
-        >
-          Test
-        </button>
-      </div>
-    </form>
+        ))
+      ) : (
+        <div>Enter a business description and click enter to generate slogans.</div>
+      )}
+    </div>
   );
 }
--- src/pages/api/icp.ts
+++ src/pages/api/icp.ts
@@ -1,33 +1,21 @@
-import type { APIRoute } from 'astro';
-import OpenAI from 'openai';
-const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
-
 export const prerender = false;
 
+import type { APIRoute } from 'astro';
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
+
 export const POST: APIRoute = async ({ request }) => {
-  try {
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      temperature: 0,
-      messages: [
-        {
-          role: 'user',
-          content: 'hello there',
-        },
-      ],
-      // ... other options
-    });
+  const { messages } = await request.json();
 
-    console.log('Completion', completion);
-
-    return new Response(JSON.stringify({ completion }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error: unknown) {
-    return new Response(JSON.stringify({ error }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: messages,
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 };
