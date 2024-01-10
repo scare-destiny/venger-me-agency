a64a1e6be47eb72b7498b519f81a72f8f9adca41
scare-destiny <zhenya.venger@gmail.com>
2024-01-10 17:12:45 +0200

    x

--- src/components/react/Tailwind/Services.tsx
+++ src/components/react/Tailwind/Services.tsx
@@ -97,7 +97,7 @@ export function Services() {
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
             // name="Starter"
-            price="Cold Email + Deliverability"
+            price="Cold Email and Linkedin + Deliverability"
             description="Connect with your audience and show them the sparkle to make your business uniquely irresistible.   "
             href="#contact"
             features={[
