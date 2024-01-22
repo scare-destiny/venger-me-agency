fac0800f52095a69b442b127194bf975893f85cb
scare-destiny <zhenya.venger@gmail.com>
2024-01-22 20:51:52 +0200

    x

--- src/components/react/UI/text-generate-effect.tsx
+++ src/components/react/UI/text-generate-effect.tsx
@@ -36,7 +36,9 @@ export const TextGenerateEffect = ({ words, className }: { words: string; classN
   return (
     <div className={cn('font-bold', className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-lg leading-snug tracking-wide">{renderWords()}</div>
+        <div className=" dark:text-white text-black text-lg leading-snug tracking-wide text-balance">
+          {renderWords()}
+        </div>
       </div>
     </div>
   );
