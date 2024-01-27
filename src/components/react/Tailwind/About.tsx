c74b818c5a7984ebd8d957c6a721a4be48e55c23
scare-destiny <zhenya.venger@gmail.com>
2024-01-27 13:57:18 +0200

    image animaate instead of div

--- src/components/react/Tailwind/About.tsx
+++ src/components/react/Tailwind/About.tsx
@@ -6,12 +6,12 @@ export default function About() {
   return (
     <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
       <div className="lg:pl-20">
-        <div className="max-w-xs px-2.5 lg:max-w-none animate-in slide-in-from-right fade-in-75  duration-1000 ">
+        <div className="max-w-xs px-2.5 lg:max-w-none  ">
           <img
             src={image.src}
             alt=""
             sizes="(min-width: 1024px) 32rem, 20rem"
-            className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800 animate-in slide-in-from-right fade-in-75  duration-1000"
           />
         </div>
       </div>
