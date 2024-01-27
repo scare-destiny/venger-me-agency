9db6794aa6f7e1c643e0465416ab3b81a99555dd
scare-destiny <zhenya.venger@gmail.com>
2024-01-27 14:00:27 +0200

    x

--- src/components/react/Tailwind/About.tsx
+++ src/components/react/Tailwind/About.tsx
@@ -11,16 +11,16 @@ export default function About() {
             src={image.src}
             alt=""
             sizes="(min-width: 1024px) 32rem, 20rem"
-            className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800 animate-in slide-in-from-right fade-in-75  duration-1000"
+            className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800 animate-in slide-in-from-right fade-in-50  duration-1000"
           />
         </div>
       </div>
       <div className="lg:order-first lg:row-span-2">
-        <FadeIn>
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Eugene Venger. I help businesses get to the next level.
-          </h1>
-        </FadeIn>
+        {/* <FadeIn> */}
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          I’m Eugene Venger. I help businesses get to the next level.
+        </h1>
+        {/* </FadeIn> */}
         <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
           <p>
             I began my journey in 2019 at a SaaS company, AmazeOwl, as a Growth Marketer. This role marked my initiation
