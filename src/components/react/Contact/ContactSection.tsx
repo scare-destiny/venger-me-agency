40546cb8122b86dcec4fc4b6270fd1c8d998e4f8
scare-destiny <zhenya.venger@gmail.com>
2024-02-09 20:44:38 +0200

    new CTA

---
+++ src/components/react/Contact/ContactSection.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button } from '~/components/react/Tailwind/Services/Button';
+import { Container } from '~/components/react/Tailwind/Container';
+import { FadeIn } from '~/components/react/Animations/FadeIn';
+import { Offices } from '~/components/react/Contact/Offices';
+import { GridList } from '~/components/react/Tailwind/GridList';
+import { GridListItem } from '~/components/react/Tailwind/GridList';
+
+export function ContactSection() {
+  return (
+    <Container className=" py-12 md:py-20 lg:py-24 ">
+      <FadeIn className="-mx-6 rounded-[40px] bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12">
+        <div className="mx-auto max-w-4xl">
+          <div className="max-w-xl">
+            <h2 className="font-display text-3xl font-bold text-white [text-wrap:balance] sm:text-4xl">
+              Tell me about your project
+            </h2>
+            <div className="mt-6 flex text-lg">
+              <Button variant="solid" color="white" outline="white" href="/contact" invert>
+                Say Hei
+              </Button>
+            </div>
+            {/* <div className="mt-10 border-t border-white/10 pt-10">
+              <h3 className="font-display text-xl font-extrabold text-white">Our offices</h3>
+              <Offices invert className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 font-bold text-lg" />
+            </div> */}
+          </div>
+          <Container className="mt-16">
+            <GridList>
+              <GridListItem title="No strings attached" invert>
+                Let's discuss your project and see if we are a good fit for each other.
+              </GridListItem>
+              <GridListItem title="Gain clarity" invert>
+                I will help you understand what you need and how to you achieve it, step by step.
+              </GridListItem>
+              <GridListItem title="Stop feeling uncertain" invert>
+                Get a clear, actionable plan to achieve your goals.
+              </GridListItem>
+            </GridList>
+          </Container>
+        </div>
+      </FadeIn>
+    </Container>
+  );
+}
--- src/components/react/Tailwind/Services/Button.tsx
+++ src/components/react/Tailwind/Services/Button.tsx
@@ -12,14 +12,12 @@ const variantStyles = {
     slate:
       'bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900',
     blue: 'bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600',
-    white:
-      'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white',
+    white: 'bg-white text-slate-900 bg-white text-neutral-950 hover:bg-neutral-500',
   },
   outline: {
     slate:
-      'ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
-    white:
-      'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
+      'ring-slate-200 text-slate-700  active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
+    white: 'ring-slate-700 text-white  active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
   },
 };
 
